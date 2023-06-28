3202a3e5b401ab598237e2c008ab5c5041e83630
nancy <smithnancy@outlook.com>
2023-06-28 16:15:53 +1000

    form component has been created and  import at app

---
+++ src/components/Form.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="md:w-1/2">
+        <h2 class=" font-black text-3xl text-center">Patients Follow up</h2>
+        <p class=" text-lg m-t-5 text-center mb-10">Add patients <span class=" text-indigo-600 font-bold">manage them</span> </p>
+    </div>
+</template>
