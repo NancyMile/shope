147f592777fa2b71f9ef9cc9b0a7686717471c7c
nancy <smithnancy@outlook.com>
2023-06-28 22:25:02 +1000

    dinamic  alert depending the type of message using computed and isError

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,15 +1,24 @@
 <script setup>
- const props = defineProps({
-    alert:{
-        type: Object,
-        required: true
-    }
- })
+    import { computed } from 'vue';
+
+    const props = defineProps({
+        alert:{
+            type: Object,
+            required: true
+        }
+    })
+
+    const isError = computed(() =>{
+        //return " this is the error function" // includes this on the class
+        return props.alert.type === 'error'
+    })
+
 </script>
 
 <template>
     <div
-        class=" text-white text-center p-3 font-bold mb-3 rounded-md bg-red-500">
+        :class="[isError ? 'bg-red-500':'bg-green-500']"
+        class=" text-white text-center p-3 font-bold mb-3 rounded-md">
         {{ alert.message }}
     </div>
 </template>
