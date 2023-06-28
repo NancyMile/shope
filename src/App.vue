74ce762b540591857343b10aff1370603823d08e
nancy <smithnancy@outlook.com>
2023-06-28 22:37:28 +1000

    app listing patients,displaying  not patients message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
+import { ref } from 'vue';
 import Header from './components/Header.vue'
 import Form from './components/Form.vue'
+
+  const patients = ref([])
+
 </script>
 
 <template>
@@ -8,6 +12,15 @@ import Form from './components/Form.vue'
     <Header/>
     <div class="mt-12 md:flex">
       <Form/>
+
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">Admin Patients</h3>
+        <div v-if="patients.length > 0 ">
+
+
+        </div>
+        <p v-else class="mt-20 text-2xl text-center"> No patients.</p>
+      </div>
     </div>
   </div>
 </template>
